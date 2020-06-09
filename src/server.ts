f674f99f733f962b01a24691f44557748acff451
Ayush Mishra <ayushm017@gmail.com>
2020-06-09 14:18:36 +0530

    explicitly stated port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import { DB_HOST, PORT } from "./configuration/env.configurer";
 
 import app from "./app";
 
-export const server = app.listen(PORT, () => {
-	console.log(`Express server running on http://${DB_HOST}:${PORT}`);
+export const server = app.listen(process.env.PORT, () => {
+	console.log(
+		`Express server running on http://${DB_HOST}:${process.env.PORT}`
+	);
 });
