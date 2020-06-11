9b1d2aa9797cc3f8fc0a5b86272ae8731882003e
Ayush Mishra <ayushm017@gmail.com>
2020-06-11 17:25:06 +0530

    Added several middleware layers

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = require("./logger/logger");
 const router_1 = require("./middleware/router/router");
+const compression_1 = __importDefault(require("compression"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
+const isProduction = process.env.NODE_ENV === "production";
+/*
+const origin = {
+    origin: isProduction ? "https://ayushm.dev" : "*"
+};
+*/
+const limiter = express_rate_limit_1.default({
+    windowMs: 1 * 60 * 1000,
+    max: isProduction ? 5 : 1000 // 5 Requests
+});
+// Rate limit specific endpoint:
+/*
+const postLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+})
+
+app.post('/books', postLimiter, addBook)
+ */
 class App {
     constructor() {
         this.app = express_1.default();
@@ -19,6 +41,9 @@ class App {
         this.app.use(express_1.default.json());
         this.app.use(express_1.default.urlencoded({ extended: true }));
         this.app.use(cors_1.default());
+        this.app.use(compression_1.default());
+        this.app.use(helmet_1.default());
+        this.app.use(limiter);
     }
 }
 exports.default = new App().app;
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
 import { Logger } from "./logger/logger";
 import { Router } from "./middleware/router/router";
+import compression from "compression";
 import cors from "cors";
 import express from "express";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+const isProduction = process.env.NODE_ENV === "production";
+/* 
+const origin = {
+	origin: isProduction ? "https://ayushm.dev" : "*"
+}; 
+*/
+
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minute
+	max: isProduction ? 5 : 1000 // 5 Requests
+});
+
+// Rate limit specific endpoint:
+
+/* 
+const postLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+})
+
+app.post('/books', postLimiter, addBook)
+ */
 
 class App {
 	public app: express.Application;
@@ -20,6 +46,9 @@ class App {
 		this.app.use(express.json());
 		this.app.use(express.urlencoded({ extended: true }));
 		this.app.use(cors());
+		this.app.use(compression());
+		this.app.use(helmet());
+		this.app.use(limiter);
 	}
 }
 
