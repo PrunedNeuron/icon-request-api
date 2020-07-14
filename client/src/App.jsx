5e1d6fd72dece2355f4f1c8c14c8c210d62e45cf
Ayush Mishra <ayushm017@gmail.com>
2020-07-14 15:40:25 +0530

    Removed client and simplified package.json

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import HomePage from "./components/HomePage/HomePage";
-import "./static/css/App.css";
-
-export default class App extends Component {
-	render() {
-		return <HomePage />;
-	}
-}
--- client/src/components/HomePage/HomePage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import LoginPage from "../LoginPage/LoginPage";
-
-export default function HomePage() {
-	return (
-		<LoginPage />
-	);
-}
